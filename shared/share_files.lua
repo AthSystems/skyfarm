0c9f725df912d7b87285c76430526a2d93a9597c
AthSystems <ath.systems@proton.me>
2025-06-07 16:46:25 +0800

    Create share_files.lua

---
+++ shared/share_files.lua
@@ -0,0 +1,29 @@
+--- Share files Script
+--- Share files from server to client computers
+--- Created by judea.
+--- DateTime: 7/06/2025 4:44 pm
+---
+
+local modem = peripheral.find("modem") or error("No modem found")
+rednet.open(peripheral.getName(modem))
+
+local modules = {
+    config = "<config-lua-string>",
+    logging = "<logging-lua-string>",
+    network = "<network-lua-string>",
+    utils = "<utils-lua-string>"
+}
+
+for k, v in pairs(modules) do
+    modules[k] = textutils.unserialize(v) or v
+end
+
+print("📡 Module server ready.")
+while true do
+    local id, msg, protocol = rednet.receive("sky-share")
+    if msg and modules[msg] then
+        rednet.send(id, modules[msg], "sky-share")
+    else
+        rednet.send(id, nil, "sky-share")
+    end
+end
