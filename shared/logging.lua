355ff6a5893fcd7cd36c29f8056cc9ef583746e7
AthSystems <ath.systems@proton.me>
2025-06-07 21:07:04 +0800

    Update logging.lua

--- shared/logging.lua
+++ shared/logging.lua
@@ -1,20 +1,50 @@
 --- Skyfarm Logging
+--- Supports log, warn, and error levels via rednet broadcast
 --- Created by judea.
 --- DateTime: 7/06/2025 4:42 pm
 ---
 
 local config = require("config")
 
-local function log(sender, msg, data)
+-- Format & print message to local terminal
+local function prompt(msg)
     local time = os.date("%H:%M:%S")
-    local label = sender or "master"
-    print(("[" .. time .. "] " .. msg))
-    rednet.send(config.ids.monitor, {
-        source = label,
-        time = time,
-        message = msg,
-        data = data
-    }, config.protocols.logs)
+    print(string.format("[%s] %s", time, msg))
 end
 
-return log
+local function send_log(level, sender, msg, data)
+    local time = os.date("%H:%M:%S")
+    local label = sender or config.node_name or "unknown"
+
+    local payload = {
+        source = label,
+        time = time,
+        level = level,
+        message = msg,
+        data = data
+    }
+
+    -- Print locally
+    print(string.format("[%s] [%s] %s", time, level:upper(), msg))
+
+    -- Broadcast to all listening nodes
+    rednet.broadcast(payload, config.protocols.logs)
+end
+
+local function log(msg, data)
+    send_log("log", config.node_name, msg, data)
+end
+
+local function warn(msg, data)
+    send_log("warn", config.node_name, msg, data)
+end
+
+local function error(msg, data)
+    send_log("error", config.node_name, msg, data)
+end
+
+return {
+    log = log,
+    warn = warn,
+    error = error
+}
